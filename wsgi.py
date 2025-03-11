107024692a55b1b2323ee5bacb67adcbf463fd00
deyanecast <deyanecast@gmail.com>
2025-03-11 00:23:35 -0600

    Add WSGI file and update Render configuration

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from run import app
+from main import app
+
+# Este archivo es el punto de entrada para Gunicorn
+if __name__ == "__main__":
+    app.run()
 
 # Vercel busca una variable llamada 'app' para el despliegue
 # Este archivo es necesario para que Vercel pueda encontrar la aplicación Flask 
